2ee8c0adadd8c9b9cdeb5b4174c6a08eaed8eb92
chei7tain <fredrickokwuobi@gmail.com>
2022-04-07 16:05:54 +0100

    feat:add event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'passwordgenerator';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  password = '';
+  useLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  useNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  useSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    this.password = Math.random().toString(36).slice(-8);
+    console.log(this.includeLetters);
+    console.log('for Numbers: ' + this.includeNumbers);
+    console.log('for Symbols: ' + this.includeSymbols);
+  }
+  getPassword() {
+    return this.password;
+  }
 }
